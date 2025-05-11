8821aae694e188327a44a7118f1ff5c2a22ff1d8
mariacedersten <s2401554@edu.bc.fi>
2025-05-11 21:23:27 +0300

    Tehty

---
+++ types/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public partial class Program
+{
+
+    public static void Main(string[] args)
+    {
+        //Kokonaisluku muuttuja//
+        int luku1;
+        // muuttujalle asetetaan arvo//
+        luku1 = 52;
+        // muuttujan arvo tulestetaan//
+        Console.WriteLine(luku1);
+
+        luku1 = 100 + 2;
+        Console.WriteLine(luku1);
+
+        //Kysy käyttäjältä tämän lempinumero//
+        int lempinumero;
+        Console.WriteLine("Kirjoita lempilukusi!");
+        string vastaus;
+        vastaus = Console.ReadLine();
+
+        //Tulkitaan vastaus kokonaisluvuksi//
+        Int32.TryParse(vastaus, out lempinumero);
+
+        //Tulkitaan , tapa 2
+        lempinumero = Convert.ToInt32(vastaus);
+
+        Console.WriteLine("Vastasit: " + (lempinumero + 5));
+    }
+}
